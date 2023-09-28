d6c59cd72364463d63f53b963da6219d4f2d49c5
Maxime Fourna <65597525+maxfrn65@users.noreply.github.com>
2023-09-28 11:40:21 +0200

    feat login form + user class

--- migrations/Version20230928092904.php
+++ migrations/Version20230928092904.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20230926064608 extends AbstractMigration
+final class Version20230928092904 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,8 +20,7 @@ final class Version20230926064608 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE nationality (id INT AUTO_INCREMENT NOT NULL, nationality VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('ALTER TABLE actor ADD nationality_id INT NOT NULL');
+        $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, email VARCHAR(180) NOT NULL, roles JSON NOT NULL, password VARCHAR(255) NOT NULL, UNIQUE INDEX UNIQ_8D93D649E7927C74 (email), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('ALTER TABLE actor ADD CONSTRAINT FK_447556F91C9DA55 FOREIGN KEY (nationality_id) REFERENCES nationality (id)');
         $this->addSql('CREATE INDEX IDX_447556F91C9DA55 ON actor (nationality_id)');
         $this->addSql('ALTER TABLE movie CHANGE description description VARCHAR(255) DEFAULT NULL, CHANGE release_date release_date DATETIME DEFAULT NULL, CHANGE duration duration VARCHAR(255) DEFAULT NULL');
@@ -30,10 +29,9 @@ final class Version20230926064608 extends AbstractMigration
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP TABLE user');
         $this->addSql('ALTER TABLE actor DROP FOREIGN KEY FK_447556F91C9DA55');
-        $this->addSql('DROP TABLE nationality');
         $this->addSql('DROP INDEX IDX_447556F91C9DA55 ON actor');
-        $this->addSql('ALTER TABLE actor DROP nationality_id');
         $this->addSql('ALTER TABLE movie CHANGE description description VARCHAR(255) NOT NULL, CHANGE release_date release_date DATE NOT NULL, CHANGE duration duration INT NOT NULL');
     }
 }
