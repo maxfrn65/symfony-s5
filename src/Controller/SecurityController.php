3a4bcf65b7566885830d4d44b5e27db3ef90a8f2
Maxime Fourna <65597525+maxfrn65@users.noreply.github.com>
2023-11-22 15:09:25 +0100

    chore: phpStan compliant + github action

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,10 +12,10 @@ use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 class SecurityController extends AbstractController
 {
     #[Route(path: '/login', name: 'app_login', methods: ['POST'])]
-    public function login(#[CurrentUser] $user = null): Response
+    public function login(#[CurrentUser] User $user = null): Response
     {
         return $this->json([
-            'user' => $user ? $user->getId() : null
+            'user' => $user?->getId()
         ]);
     }
 
