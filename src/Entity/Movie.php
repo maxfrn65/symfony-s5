fde808fa131b34ddd7f332795508b95f4e80caad
Maxime Fourna <65597525+maxfrn65@users.noreply.github.com>
2023-12-12 17:29:03 +0100

    feat: movie api filter

--- src/Entity/Movie.php
+++ src/Entity/Movie.php
@@ -15,7 +15,7 @@ use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: MovieRepository::class)]
 #[ApiResource()]
-#[ApiFilter(SearchFilter::class, properties: ['id' => 'exact', 'title' => 'partial'])]
+#[ApiFilter(SearchFilter::class, properties: ['title' => 'partial'])]
 class Movie
 {
     #[ORM\Id]
