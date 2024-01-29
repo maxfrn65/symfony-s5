014bea3ba3a5ba701f350e99df5aa3733081c63b
Maxime Fourna <65597525+maxfrn65@users.noreply.github.com>
2024-01-29 14:48:48 +0100

    feat: actors fixtures w/ fakerphp

--- src/DataFixtures/ActorFixtures.php
+++ src/DataFixtures/ActorFixtures.php
@@ -7,20 +7,21 @@ use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 use Doctrine\Persistence\ObjectManager;
 use App\Entity\Actor
     ;
+use Faker\Factory;
 
 class ActorFixtures extends Fixture implements DependentFixtureInterface
 {
     public function load(ObjectManager $manager): void
     {
 
-        $firstName = ['Jean','Pierre','Paul','Jacques','Marie','Julie','Julien','Jeanne','Pierre','Pauline','Marie'];
-        $lName = ['Dupont','Durand','Duchemin','Duchesse','Duc','Ducroc','Ducrocq','Ducroq','Ducro','Ducros','Ducroix'];
-
+        // $firstName = ['Jean','Pierre','Paul','Jacques','Marie','Julie','Julien','Jeanne','Pierre','Pauline','Marie'];
+        // $lName = ['Dupont','Durand','Duchemin','Duchesse','Duc','Ducroc','Ducrocq','Ducroq','Ducro','Ducros','Ducroix'];
+        $faker = Factory::create('en_US');
         // Génère moi 10 objets Actor fictifs dans un foreach
         foreach (range(1, 10) as $i) {
             $actor = new Actor();
-            $actor->setFirstName($firstName[$i]);
-            $actor->setLastName($lName[$i]);
+            $actor->setFirstName($faker->firstName());
+            $actor->setLastName($faker->lastName());
             $actor->setNationality($this->getReference('nationality_' . rand(1, 9)));
             $manager->persist($actor); // "expose" l'objet à Doctrine pour qu'il soit enregistré en BDD
             $this->addReference('actor_' . $i, $actor);
